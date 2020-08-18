01978bbb65b5b1596f3538623df0b2a027caa09b
Salutesh <stephan_moser@gmx.de>
2020-08-18 13:44:31 +0200

    Mission files 1.3.1044

---
+++ ExpansionCOM.ChernarusPlus/init.c
@@ -0,0 +1,44 @@
+#define EXPANSION_LOOT_SPAWN_POSITION_THINGY
+
+#include "$CurrentDir:\\Missions\\ExpansionCOM.ChernarusPlus\\core\\BaseModuleInclude.c"
+
+#include "$CurrentDir:\\Missions\\ExpansionCOM.ChernarusPlus\\expansion\\ExpansionObjectSpawnTools.c"
+#include "$CurrentDir:\\Missions\\ExpansionCOM.ChernarusPlus\\expansion\\missions\\MissionConstructor.c"
+
+Mission CreateCustomMission(string path)
+{
+	return new CommunityOfflineClient();
+}
+
+typedef string CFString;
+
+class CFString: string
+{
+	void Test()
+	{
+		Print( string.Format( "Test: %1", value ) );
+	}
+};
+
+void main()
+{
+	CFString test_string = "Hello";
+	test_string.Test();
+
+	bool loadTraderObjects = false;
+	bool loadTraderNPCs = false;
+
+	string MissionWorldName = "empty";
+	GetGame().GetWorldName(MissionWorldName);
+	
+	if (MissionWorldName != "empty")
+	{
+		//! Spawn mission objects and traders
+		FindMissionFiles(MissionWorldName, loadTraderObjects, loadTraderNPCs);
+	}
+	
+	//! Init server central economy
+	// Hive ce = CreateHive();
+	// if ( ce )
+	// 	ce.InitOffline();
+}
---
+++ README.txt
@@ -0,0 +1,4 @@
+Missions with "COM" in their name are for offline mode !
+
+After each expansion updates, please update your expansionmod server settings !
+Our wiki should be able to help you : https://github.com/salutesh/DayZ-Expansion-Scripts/wiki
