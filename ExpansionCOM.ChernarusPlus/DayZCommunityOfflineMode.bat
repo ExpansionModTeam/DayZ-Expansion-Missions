931124e373ccace9bd9613b02140a0b56e5baf3f
LieutenantMaster <jouve10>
2020-11-26 18:21:46 +0100

    Expansion Missions 1.5.1155
    Updated COM bath files (changed mod order

--- ExpansionCOM.ChernarusPlus/DayZCommunityOfflineMode.bat
+++ ExpansionCOM.ChernarusPlus/DayZCommunityOfflineMode.bat
@@ -6,4 +6,4 @@ RD /s /q "storage_-1" > nul 2>&1
 
 cd ../../
 
-start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.ChernarusPlus -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=!Workshop\@DayZ-Expansion-;!Workshop\@DayZ-Expansion-Licensed-;!Workshop\@DayZ-Expansion-Vehicles-;!Workshop\@DayZ-Expansion-Core-;!Workshop\@CF;!Workshop\@Community-Online-Tools;"
+start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.ChernarusPlus -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=!Workshop\@CF;!Workshop\@Community-Online-Tools;!Workshop\@DayZ-Expansion-;!Workshop\@DayZ-Expansion-Licensed-;!Workshop\@DayZ-Expansion-Vehicles-;!Workshop\@DayZ-Expansion-Core-;"
