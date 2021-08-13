0e4175d691d5a3556d621e065efd20d1c9e6d7fe
Steve <stephan_moser@gmx.de>
2021-08-13 21:34:07 +0200

    Added cleaned Expansion Events XMLs and fixed offline mission bat files

--- Takistan/ExpansionCOM.TakistanPlus/DayZCommunityOfflineMode.bat
+++ Takistan/ExpansionCOM.TakistanPlus/DayZCommunityOfflineMode.bat
@@ -6,4 +6,4 @@ RD /s /q "storage_-1" > nul 2>&1
 
 cd ../../
 
-start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.TakistanPlus -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=P:/Mods/@Community-Framework;P:/Mods/@Community-Online-Tools;P:/Mods/@Dabs-Framework;P:/Mods/@DayZ-Expansion-Core;P:/Mods/@DayZ-Expansion;P:/Mods/@DayZ-Expansion-Vehicles;P:/Mods/@DayZ-Expansion-Licensed;P:/Mods/@DayZ-Expansion-Book;P:/Mods/@DayZ-Expansion-Market;P:/Mods/@TakistanPlus"
+start DayZ_x64.exe -mission=.\Missions\ExpansionCOM.TakistanPlus -nosplash -noPause -noBenchmark -filePatching -doLogs -scriptDebug=true "-mod=!Workshop\@CF;!Workshop\@Community-Online-Tools;!Workshop\@Dabs Framework;!Workshop\@DayZ-Expansion-Bundle;!Workshop\@DayZ-Expansion-Licensed";!Workshop\@TakistanPlus"
